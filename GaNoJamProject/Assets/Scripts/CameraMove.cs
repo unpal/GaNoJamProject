9e877601b84117be046d15c162a76344813ac4d8
unpal <83804415+unpal@users.noreply.github.com>
2026-05-12 23:37:45 +0900

    카메라 무브 시스템 추가
    변경점 : 카메라가 부드럽게 움직일 수 있도록 카메라 무브 시스템을 추가했습니다.
    
    해야할 일 : 각각의 배경이 나왔을 때 카메라를 부드럽게 변경되도록 테스트 및 결과에 따른 변경

---
+++ GaNoJamProject/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject FruitsChoiceMain;
+    public GameObject MainImage;
+    public int ImageType;
+    private Vector3 velocity = Vector3.zero;
+    public float speed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 tempPosition;
+        switch (ImageType)
+        {
+            case 0:
+                {
+                    tempPosition = FruitsChoiceMain.transform.position;
+                    tempPosition.z = transform.position.z;
+                }
+                break;
+            case 1:
+                {
+                    tempPosition = MainImage.transform.position;
+                    tempPosition.z = transform.position.z;
+                }
+                break;
+            default:
+                {
+                    tempPosition = MainImage.transform.position;
+                    tempPosition.z = transform.position.z;
+                }
+                break;
+        }
+       transform.position = Vector3.SmoothDamp(gameObject.transform.position, tempPosition, ref velocity, speed);
+    }
+}
